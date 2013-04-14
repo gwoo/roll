b6ef380413b9f6e03bcf42d19eec5fd1baba09b5
gwoo <gwoohoo@gmail.com>
2013-04-14 14:49:15 -0700

    Adding clusters and ability to execute individual scripts.

--- lib/szoo/cli.rb
+++ lib/szoo/cli.rb
@@ -10,10 +10,17 @@ module SZoo
       do_create(project)
     end
 
-    desc 'deploy [user@host:port] [role] [--sudo]', 'Deploy szoo project'
+    desc 'deploy [--sudo] cluster|host|user@host:port [role]', 'Deploy szoo project on the host(s)'
     method_options :sudo => false
-    def deploy(target, role = nil)
+    def deploy(target = nil, role = nil)
       do_deploy(target, role, options.sudo?)
+    end
+
+    desc 'execute [--sudo] cluster|host|user@host:port script', 'Execute a script on the host(s).'
+    method_options :sudo => false
+    def execute(target = nil, script)
+      script = "scripts/#{script}"
+      do_execute(target, script, options.sudo?)
     end
 
     desc 'compile', 'Compile szoo project'
@@ -55,55 +62,58 @@ module SZoo
         copy_file 'templates/create/files/.gitkeep',     "#{project}/files/.gitkeep"
       end
 
-      def do_deploy(target, role, force_sudo)
+      def do_execute(target, script, force_sudo)
+        get_config()
         sudo = 'sudo ' if force_sudo
-        user, host, port = parse_target(target)
-        endpoint = "#{user}@#{host}"
+        hosts(target).each do |target|
+          user, host, port = parse_target(target)
+          endpoint = "#{user}@#{host}"
 
-        # compile attributes and scripts
-        do_compile(role)
+          `ssh-keygen -R #{host} 2> /dev/null`
 
-        # The host key might change when we instantiate a new VM, so
-        # we remove (-R) the old host key from known_hosts.
-        `ssh-keygen -R #{host} 2> /dev/null`
+          remote_commands = <<-EOS
+          rm -rf ~/szoo &&
+          mkdir ~/szoo &&
+          cd ~/szoo &&
+          tar xz &&
+          #{sudo}bash #{script}
+          EOS
 
-        remote_commands = <<-EOS
-        rm -rf ~/szoo &&
-        mkdir ~/szoo &&
-        cd ~/szoo &&
-        tar xz &&
-        #{sudo}bash install.sh
-        EOS
+          remote_commands.strip! << ' && rm -rf ~/szoo' if @config['preferences'] and @config['preferences']['erase_remote_folder']
 
-        remote_commands.strip! << ' && rm -rf ~/szoo' if @config['preferences'] and @config['preferences']['erase_remote_folder']
+          local_commands = <<-EOS
+          cd compiled
+          tar cz . | ssh -o 'StrictHostKeyChecking no' #{endpoint} -p #{port} '#{remote_commands}'
+          EOS
 
-        local_commands = <<-EOS
-        cd compiled
-        tar cz . | ssh -o 'StrictHostKeyChecking no' #{endpoint} -p #{port} '#{remote_commands}'
-        EOS
-
-        Open3.popen3(local_commands) do |stdin, stdout, stderr|
-          stdin.close
-          t = Thread.new do
-            while (line = stderr.gets)
-              print line.color(:red)
+          Open3.popen3(local_commands) do |stdin, stdout, stderr|
+            stdin.close
+            t = Thread.new do
+              while (line = stderr.gets)
+                print line.color(:red)
+              end
             end
+            while (line = stdout.gets)
+              print line.color(:green)
+            end
+            t.join
           end
-          while (line = stdout.gets)
-            print line.color(:green)
-          end
-          t.join
         end
       end
 
+      def do_deploy(target, role, force_sudo)
+        # compile attributes and scripts
+        do_compile(role)
+
+        # run the install script
+        do_execute(target, "install.sh", force_sudo)
+      end
+
       def do_compile(role)
-        # Check if you're in the szoo directory
-        abort_with 'You must be in the szoo folder' unless File.exists?('szoo.yml')
+        get_config()
+
         # Check if role exists
         abort_with "#{role} doesn't exist!" if role and !File.exists?("roles/#{role}.sh")
-
-        # Load szoo.yml
-        @config = YAML.load(File.read('szoo.yml'))
 
         # Break down attributes into individual files
         (@config['attributes'] || {}).each {|key, value| create_file "compiled/attributes/#{key}", value }
@@ -122,6 +132,8 @@ module SZoo
         Dir['roles/*'].each   {|file| send copy_or_template, File.expand_path(file), "compiled/roles/#{File.basename(file)}" }
         Dir['files/*'].each   {|file| send copy_or_template, File.expand_path(file), "compiled/files/#{File.basename(file)}" }
         (@config['files'] || []).each {|file| send copy_or_template, File.expand_path(file), "compiled/files/#{File.basename(file)}" }
+        send copy_or_template, File.expand_path('szoo.sh'), 'compiled/szoo.sh'
+        send copy_or_template, File.expand_path('config.sh'), 'compiled/config.sh'
 
         # Build install.sh
         if role
@@ -134,6 +146,36 @@ module SZoo
         else
           send copy_or_template, File.expand_path('install.sh'), 'compiled/install.sh'
         end
+      end
+
+      def get_config()
+        # Check if you're in the szoo directory
+        abort_with 'You must be in the szoo folder' unless File.exists?('szoo.yml')
+
+        # Load szoo.yml
+        @config = YAML.load(File.read('szoo.yml'))
+      end
+
+      def hosts(input)
+        hosts = nil
+        if !@config || !@config.has_key?('clusters') then
+          return [input]
+        end
+        @config['clusters'].each {|key,value|
+          if key == input then
+            hosts=value
+            break
+          end
+          if value.is_a? Hash
+            value.each {|key, value|
+              if key == input then
+                hosts={key:value}
+                break
+              end
+            }
+          end
+        }
+        return hosts == nil ? [input] : hosts.values
       end
 
       def parse_target(target)
